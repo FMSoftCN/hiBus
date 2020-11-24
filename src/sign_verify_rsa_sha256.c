f8c28bd53d639506706e147692451cb4514f5cd4
Vincent Wei <vincent@minigui.org>
2020-11-24 17:16:47 +0800

    The RSA/SHA256 version of hibus_sign_challenge_code and hibus_verify_signature

---
+++ src/sign_verify_rsa_sha256.c
@@ -0,0 +1,157 @@
+/*
+** sign_verify_rsa_sha256.c -- The utilitis for sign the challenge code
+**  and verify the signature with RSA and SHA256 algorithms.
+**
+** Copyright (c) 2020 FMSoft (http://www.fmsoft.cn)
+**
+** Author: Vincent Wei (https://github.com/VincentWei)
+**
+** This file is part of hiBus.
+**
+** hiBus is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** hiBus is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+#include <string.h>
+#include <errno.h>
+
+#include <hibox/ulog.h>
+
+#ifdef HAVE_LIBSSL
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <openssl/sha.h>
+#include <openssl/crypto.h>
+
+#include "hibus.h"
+
+static int my_error_printer (const char *str, size_t len, void *u)
+{
+    ULOG_ERR ("%s\n", str);
+    return 0;
+}
+
+static RSA* read_private_key_for_app (const char* app_name)
+{
+    int size;
+    char buff [512];
+    FILE *fp = NULL;
+    RSA *pri_key = NULL;
+
+    size = snprintf (buff, 512, HIBUS_PRIVATE_PEM_KEY_FILE, app_name, app_name);
+    if (size >= sizeof (buff)) {
+        ULOG_ERR ("Too long app name in read_private_key_for_app: %s\n", app_name);
+        return NULL;
+    }
+
+    if ((fp = fopen (buff, "r")) == NULL) {
+        ULOG_ERR ("Failed to open the private key file for app (%s): %s\n",
+                app_name, strerror (errno));
+        return NULL;
+    }
+
+    pri_key = PEM_read_RSAPrivateKey (fp, NULL, NULL, NULL);
+    if (pri_key == NULL) {
+        ULOG_ERR ("Failed to read RSA private key for app (%s):\n", app_name);
+        ERR_print_errors_cb (my_error_printer, NULL);
+    }
+
+    fclose(fp);
+    return pri_key;
+}
+
+unsigned char *hibus_sign_challenge_code (const char *app_name, const char* ch_code,
+        unsigned int *sig_len)
+{
+    RSA *priv_key = NULL;
+    unsigned char md [SHA256_DIGEST_LENGTH];
+    unsigned char *sig;
+    int retv = 0;
+
+    sig = NULL;
+    *sig_len = 0;
+
+    priv_key = read_private_key_for_app (app_name);
+    if (!priv_key) {
+        return NULL;
+    }
+
+    if ((sig = calloc (1, 128)) == NULL) {
+        goto failed;
+    }
+
+    SHA256 ((unsigned char *)ch_code, strlen (ch_code), md);
+    retv = RSA_sign (NID_sha256, md, SHA256_DIGEST_LENGTH, sig, sig_len, priv_key);
+    if (retv != 1) {
+        free (sig);
+        sig = NULL;
+        sig_len = 0;
+    }
+
+failed:
+    RSA_free (priv_key);
+    return sig;
+}
+
+static RSA* read_public_key_for_app (const char* app_name)
+{   
+    int size;
+    char buff [512];
+    FILE *fp; 
+    RSA *pub_key = NULL;
+
+    size = snprintf (buff, sizeof (buff), HIBUS_PUBLIC_PEM_KEY_FILE, app_name);
+    if (size >= sizeof (buff)) {
+        ULOG_ERR ("Too long app name in read_public_key_for_app: %s\n", app_name);
+        return NULL;
+    }
+
+    if ((fp = fopen (buff, "r")) == NULL) {
+        ULOG_ERR ("Failed to open public key file for app (%s): %s\n",
+                app_name, strerror (errno));
+        return NULL;
+    }
+
+    if ((pub_key = PEM_read_RSA_PUBKEY (fp, NULL, NULL, NULL)) == NULL) {
+        ULOG_ERR ("Failed to read RSA public key for app (%s):\n", app_name);
+        ERR_print_errors_cb (my_error_printer, NULL);
+        goto failed;
+    }
+
+failed:
+    fclose (fp);
+    return pub_key;
+}
+
+int hibus_verify_signature (const char* app_name, const char* ch_code,
+        const unsigned char* sig, unsigned int sig_len)
+{
+    unsigned char md [SHA256_DIGEST_LENGTH];
+    RSA *pub_key = NULL;
+    int retv = 0;
+
+    pub_key = read_public_key_for_app (app_name);  
+    if (!pub_key) {
+        return 0;
+    }
+
+    SHA256 ((unsigned char *)ch_code, strlen (ch_code), md);
+
+    retv = RSA_verify (NID_sha256, md, SHA256_DIGEST_LENGTH, sig, sig_len, pub_key);
+    RSA_free (pub_key);
+    return retv;
+}
+
+#endif /* HAVE_LIBSSL */
+
