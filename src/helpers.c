e7a80eb7d53586bbcb0d1fd0a5e9fdae8bfc698c
Vincent Wei <vincent@minigui.org>
2021-11-29 18:50:50 +0800

    cleanup for new hibox version

--- src/helpers.c
+++ src/helpers.c
@@ -21,6 +21,8 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
--- src/sign_verify_dummy.c
+++ src/sign_verify_dummy.c
@@ -22,6 +22,7 @@
 ** along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
